c6fcf90ac52d5c232546a9f8431fa5e0b0f6e447
Salma Maher <153623793+salmamaher14@users.noreply.github.com>
2024-05-18 17:09:59 +0300

    Add files via upload
    initial commit

---
+++ SkyPulse/SkyPulse/SkyPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyPulseApp.swift
+//  SkyPulse
+//
+//  Created by Salma on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SkyPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
